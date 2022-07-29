e5aab2d5500f063abdd9809be92542f5e4628685
Andre Luiz Thiel <43017985+andrethiel@users.noreply.github.com>
2022-07-28 22:25:47 -0300

    novo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Container } from "@chakra-ui/react";
+import ModalDisclaimer from "./Modal";
+import axios from "axios";
 
 function App() {
+  const [show, setShow] = useState(true);
+
+  const handleClose = () => setShow(false);
+
+  const busca = async () => {
+    const response = await axios.get();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ModalDisclaimer open={show} close={handleClose} />
+    </Container>
   );
 }
 
---
+++ src/Modal.js
@@ -0,0 +1,34 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  Button,
+} from "@chakra-ui/react";
+import React from "react";
+
+function ModalDisclaimer({ open, close }) {
+  return (
+    <Modal isOpen={open}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Disclaimer</ModalHeader>
+        <ModalBody>
+          As ações apresentas aqui são baseadas no metodo e não têm como fim
+          indicar se a empresa é um bom investimento ou não. E sim mostrar se a
+          empresa está barata de acordo com o metodo.
+        </ModalBody>
+
+        <ModalFooter>
+          <Button bg={"#4BD963"} _hover={{ bg: "#228b22" }}>
+            Buscar ações
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default ModalDisclaimer;
